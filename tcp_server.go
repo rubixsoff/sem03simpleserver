797ce41bdc0d07df46dc06b06562dc02f6551d1f
rubixsoff <sofia.saelim03@gmail.com>
2023-05-05 09:29:41 +0000

    sjekke feil

--- tcp_server.go
+++ tcp_server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"sync"
+	"github.com/rubixsoff/is105sem03"
 )
 
 func main() {
@@ -36,9 +37,15 @@ func main() {
 						}
 						return // fra for løkke
 					}
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+log.Println("Dekrypter melding: ", string(dekryptertMelding))
+switch msg := string(dekryptertMelding); msg { ...
+
 					switch msg := string(buf[:n]); msg {
   				        case "ping":
-						_, err = c.Write([]byte("pong"))
+						kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+log.Println("Kryptert melding: ", string(kryptertMelding))
+_, err = conn.Write([]byte(string(kryptertMelding)))
 					default:
 						_, err = c.Write(buf[:n])
 					}
